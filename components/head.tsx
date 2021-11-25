12b5083f53175d235a23ef9b46342c31b25f66ee
Matt Leong <mattleong91@gmail.com>
2021-11-25 12:31:21 -0800

    feat: new favicon

--- components/head.tsx
+++ components/head.tsx
@@ -1,14 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { default as NextHead } from "next/head";
 
 type TProps = {
-  title?: string,
-}
+  title?: string;
+};
 
-export function Head({ title = 'CosmicNvim - Home' }: TProps) {
+export function Head({ title = "CosmicNvim - Home" }: TProps) {
   useEffect(() => {
     ///@ts-ignore
-    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+    (window.heap = window.heap || []),
+      (heap.load = function (e, t) {
+        (window.heap.appid = e), (window.heap.config = t = t || {});
+        var r = document.createElement("script");
+        (r.type = "text/javascript"),
+          (r.async = !0),
+          (r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js");
+        var a = document.getElementsByTagName("script")[0];
+        a.parentNode.insertBefore(r, a);
+        for (
+          var n = function (e) {
+              return function () {
+                heap.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+              };
+            },
+            p = [
+              "addEventProperties",
+              "addUserProperties",
+              "clearEventProperties",
+              "identify",
+              "resetIdentity",
+              "removeEventProperty",
+              "setEventProperties",
+              "track",
+              "unsetEventProperty",
+            ],
+            o = 0;
+          o < p.length;
+          o++
+        )
+          heap[p[o]] = n(p[o]);
+      });
     ///@ts-ignore
     heap.load(process.env.NEXT_PUBLIC_HEAP_ID);
   }, []);
@@ -21,7 +52,7 @@ export function Head({ title = 'CosmicNvim - Home' }: TProps) {
         content="CosmicNvim is a lightweight and opinionated Neovim config for web development, specifically designed to provide a 💫 COSMIC programming experience!"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/cosmic.ico" />
+      <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
 }
